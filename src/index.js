ed16e81ae2bedab24816a3c86da9c533ca853f2a
Arnab Manna <arnabmanna203@gmail.com>
2023-04-17 14:49:54 +0530

    Learn How to use function components in react 🚀🚀

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// create a react app from scratch;
+//show a single h1 that show good morning if between midnight and 12 pm . 
+//or "Good Evening" if between 12 PM and 6 PM
+// Apply the "heading " style in the style.css
+// Dynamically change the color of the h1 using inline css styles.
+//Morning = red , Afternoon = green ,Night = blue .
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import React from "react";
+import ReactDOM  from "react-dom";
+import App from "./App";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+ReactDOM.render(
+     <App />,
+     document.getElementById("root")
+)
+ 
